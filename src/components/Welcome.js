03a7ec95843a6328b6972a94c37244a88756801e
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2026-04-03 08:04:51 +0000

    runningcode

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,12 @@
 // write code for Welcome component here
+import React from "react";
+function Welcome({name}){
+    return (
+        <>
+        <h1>Hey!{name}</h1>
+        <p>Welcome to School.</p>
+        </>
+    )
+
+}
+export default Welcome
